f4974fa312663f15ed549213c3ef47f4edb9f0d0
urnicus <ryan@urnicus.com>
2021-02-06 08:48:05 -0500

    Update payment_transaction.rb

--- payment_transaction.rb
+++ payment_transaction.rb
@@ -1,1 +1,6 @@
 
+def delete_reimport_transactions(payout_id)
+  Shopify::PaymentTransaction.where(payout_id: payout_id).delete_all
+  payout = Shopify::PaymentPayout.new payout_id: payout_id
+  Shopify::PaymentTransaction.import_shopify payout
+end
